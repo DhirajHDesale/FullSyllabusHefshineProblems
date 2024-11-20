ade10a4c7731d7e49a8f6c527774a04aad4c2cc1
Dhiraj Desale <79238778+DhirajHDesale@users.noreply.github.com>
2024-11-20 08:47:09 +0530

    Multiples Power

---
+++ MultiplesPower.java
@@ -0,0 +1,34 @@
+/*
+Multiples Power 
+Calculate the sum of all the multiples of 3 or 7 below the natural number N.
+
+Example 1:
+
+Input: 10
+Output: 25
+Explanation:
+Numbers that are multiple of 3 or 7
+are 3, 6, 7, 9 so sum will be 25.
+*/
+public class MyClass 
+{
+	
+	public static void main(String[] args) 
+	{
+		int N = 10;
+		myCode(N);
+	}
+	
+	public static void myCode(int N) 
+	{
+		int sum = 0;
+		for(int i = 1;i<=N;i++) 
+		{
+			if(i%3==0 || i%7==0)
+			{
+				sum = sum + i;
+			}
+		}
+		System.out.println(sum);
+	}
+}
