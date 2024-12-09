53078a8630ff02aaec501da6484b754834e6b0d3
Dhiraj Desale <79238778+DhirajHDesale@users.noreply.github.com>
2024-12-09 19:24:33 +0530

    sort a given array list

---
+++ sort a given array list.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyClass {
+	
+		public static void main(String[] args) 
+		{
+			  List<String> List1 = new ArrayList<String>();
+			  List1.add("x");
+			  List1.add("y");
+			  List1.add("z");
+			  List1.add("a");
+			myCode(List1);
+		}
+	
+
+	public static void myCode(List<String> list1) 
+	{
+	
+Collections.sort(list1);
+System.out.println(list1);
+		 
+	}
+}
